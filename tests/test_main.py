2f0c9367a96dca9eba0f7115301694fb23e2821e
alvesmagnocavalcante <alvesmagnocavalcante@gmail.com>
2026-09-23 22:22:48 -0300

    update

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,5 +8,5 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
 
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json() == {"status": "ok"}
